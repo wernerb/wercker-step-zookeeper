052d980df8631de25924aa557fc42595eb296663
Werner Buck <wernerbuck@gmail.com>
2013-09-03 15:21:08 +0200

    Fix error messages and fix wget 0 to O

--- run.sh
+++ run.sh
@@ -5,19 +5,19 @@ fi
 export TMP1=$(basename $WERCKER_ZOOKEEPER_TARBALL)
 export WERCKER_ZOOKEEPER_FILENAME=${TMP1%.tar.gz}
 if [ ! -f $WERCKER_CACHE_DIR/$WERCKER_ZOOKEEPER_FILENAME/bin/zkServer.sh ]; then
-  debug '$WERCKER_ZOOKEEPER_FILENAME/bin/zkServer.sh not found in cache directory, will download it'
+  debug 'ZooKeeper not found in cache directory, will download it'
   cd /tmp
   rm -f zookeeper*
-  wget -0 $WERCKER_ZOOKEEPER_FILENAME.tar.gz $WERCKER_ZOOKEEPER_TARBALL || fail 'unable to download from $WERCKER_ZOOKEEPER_TARBALL'
+  wget -O $WERCKER_ZOOKEEPER_FILENAME.tar.gz $WERCKER_ZOOKEEPER_TARBALL || fail 'unable to download tarball'
   tar -zxvf $WERCKER_ZOOKEEPER_FILENAME.tar.gz
   mv $WERCKER_ZOOKEEPER_FILENAME/conf/zoo_sample.cfg $WERCKER_ZOOKEEPER_FILENAME/conf/zoo.cfg
   cp -r $WERCKER_ZOOKEEPER_FILENAME $WERCKER_CACHE_DIR/
   cd -
-  debug '$WERCKER_ZOOKEEPER_FILENAME added to cache successfully'
+  debug 'ZooKeeper added to cache successfully'
 else
-  debug '$WERCKER_ZOOKEEPER_FILENAME found in cache directory'
+  debug 'ZooKeeper found in cache directory'
 fi
 
-debug 'Starting: $WERCKER_CACHE_DIR/$WERCKER_ZOOKEEPER_FILENAME/bin/zkServer.sh start'
+debug 'Starting ZooKeeper with /bin/zkServer.sh start'
 $WERCKER_CACHE_DIR/$WERCKER_ZOOKEEPER_FILENAME/bin/zkServer.sh start
-exec 6<>/dev/tcp/localhost/2181
+exec 6<>/dev/tcp/localhost/2181 || fail 'socket at localhost:2181 did not open'
